4fdd2f1c69af7f3c8a16b08a84982d539e15ad98
thakur999955-netizen <thakur999955@gmail.com>
2026-08-25 09:50:36 +0530

    html.docx

--- index.js
+++ index.js
@@ -51,19 +51,68 @@
 
 
 
-function login(error,msg){
-    if(error){
-        console.log("Error is:"+error)
-    }
-    else{
-        console.log(msg)
-    }
+// function login(error,msg){
+//     if(error){
+//         console.log("Error is:"+error)
+//     }
+//     else{
+//         console.log(msg)
+//     }
+// }
+// function loginHandler(username,password,clbk){
+//     if(username=="raj" && password=="12345"){
+//         clbk(null,"Login Success")
+//     }else{
+//         clbk("username or password is incorrect",null)
+//     }
+// }
+// loginHandler("raj","1245",login)
+
+
+//Synchronization
+// console.log("One")
+// console.log("Two")
+// console.log("Three")
+
+
+//asynchronous
+// console.log("One")
+// for(i=0; i<1000; i++){
+//     console.log("i= "+i);
+// }
+// setTimeout(()=>{console.log("Two")},1000)
+// console.log("Three")
+
+const container=document.getElementById('container');
+const button=document.getElementById('btn');
+
+const h1=document.createElement('h1');
+console.log(h1)
+h1.innerHTML='ABES Engineering College';
+const loader=document.createElement('h2');
+container.appendChild(loader)
+const img=document.createElement('img');
+
+// console.log(button)
+// console.log(container)
+
+function ping(){
+    try{
+        loader.innerHTML='<h2>Loading Data...</h2>'
+   // alert('server ping');
+   container.innerHTML='<h2 style=color:red>Welcome to DOM</h2>'
+   h1.style.backgroundColor='cyan';
+   h1.style.color='red';
+   container.appendChild(h1);
+   img.src="https://thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg";
+   img.setAttribute('height',200);
+   img.setAttribute('width',200);
+   container.appendChild(img);
+}catch(e){
+    loader.innerHTML='<h2 style=color:red>Error in loading data</h2>'
 }
-function loginHandler(username,password,clbk){
-    if(username=="raj" && password=="12345"){
-        clbk(null,"Login Success")
-    }else{
-        clbk("username or password is incorrect",null)
-    }
+finally{
+    loader.innerHTML='';
 }
-loginHandler("raj","1245",login)
+}
+button.addEventListener('click',ping);
